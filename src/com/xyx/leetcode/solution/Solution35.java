6750dc7449f1c6ee664fb08e9be1b3c38b85577e
'huanxyx' <'731705638@qq.com'>
2018-05-14 15:14:52 +0800

    35，39，40

---
+++ src/com/xyx/leetcode/solution/Solution35.java
@@ -0,0 +1,19 @@
+package com.xyx.leetcode.solution;
+
+// 35.Search Insert Position
+public class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        
+        while (left <= right) {
+        	int mid = (right + left) >> 1;
+        	if (nums[mid] == target) 
+        		return mid;
+        	else if (nums[mid] < target)
+        		left = mid + 1;
+        	else 
+        		right = mid - 1;
+        }
+        return left;
+    }
+}
---
+++ src/com/xyx/leetcode/solution/Solution40.java
@@ -0,0 +1,33 @@
+package com.xyx.leetcode.solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 40.Combination Sum II
+public class Solution40 {
+	public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		Arrays.sort(candidates);
+		getResult(candidates, target, 0, list, new ArrayList<>());
+		return list;
+	}
+	
+	private void getResult(int[] candidates, 
+			int target, int start, List<List<Integer>> list, List<Integer> temp) {
+		 if (target == 0) {
+			 list.add(new ArrayList<>(temp));
+		 } else if (target > 0) {
+			 //因为是已经排好序的序列
+			 //所以当放入当前值不符合要求的时候，就无需考虑之后的部分了。
+			 for (int i = start; i < candidates.length && target-candidates[i] >= 0; i++) {
+				 //考虑过当前元素了之后就不用再考虑相同的元素了，为了避免重复解
+				 if (i > start && candidates[i] == candidates[i-1])
+					 continue;
+				 temp.add(candidates[i]);
+				 getResult(candidates, target - candidates[i], i+1, list, temp);
+				 temp.remove(temp.size()-1);
+			 }
+		 }
+	}
+}
