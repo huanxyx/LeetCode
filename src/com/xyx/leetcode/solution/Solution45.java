99ddd9cffd5fe3a3b5510c15ee2b0daf3a1358ff
'huanxyx' <'731705638@qq.com'>
2018-05-15 16:06:06 +0800

    41,42,45,48,53

---
+++ src/com/xyx/leetcode/solution/Solution45.java
@@ -0,0 +1,25 @@
+package com.xyx.leetcode.solution;
+
+// 45.Jump Game II
+public class Solution45 {
+	
+	/*
+	 * 贪婪算法:求出每步能够走到的最远距离
+	 */
+	public int jump(int[] nums) {
+        int jumps = 0;
+        int lastEnd = 0;
+        int curEnd = 0;
+        for (int i = 0; i < nums.length; ++i) {
+        	//当前位置超过了jumps步能够跳到的最远位置时，jumps+1
+            if (i > lastEnd) {
+                lastEnd = curEnd;
+                jumps++;
+            }
+            //存储着走到当前位置的过程中能够跳到的最远位置。
+            curEnd = Math.max(curEnd, i+nums[i]);
+        }
+		
+		return jumps;
+	}
+}
